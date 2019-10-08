e0cf874f97a6703742bb23981e4f79a15f7ccf0d
Jesus Moreno <jesus.moreno@janaq.com>
2019-10-07 21:11:35 -0500

    [IMP] agrega nuevas migraciones

---
+++ database/migrations/2019_10_08_012048_create_post_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title',255);
+            $table->string('brief',100);
+            $table->text('content');
+            $table->boolean('status');
+            $table->string('slug');
+            //relaciones
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('category');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
